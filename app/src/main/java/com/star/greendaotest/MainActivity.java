142873f8699e484d9350e82ebf049d2aaa00d16c
crystalDf <chendong333@gmail.com>
2018-11-18 21:16:50 +0800

    GreenDaoTest

--- app/src/main/java/com/star/greendaotest/MainActivity.java
+++ app/src/main/java/com/star/greendaotest/MainActivity.java
@@ -1,7 +1,8 @@
 package com.star.greendaotest;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initGreenDao() {
+
+
     }
 }
