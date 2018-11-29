ca2eb83331a9cc2521118431ba19e09f84a455a3
crystalDf <chendong333@gmail.com>
2018-11-29 23:49:04 +0800

    GreenDaoTest

--- app/src/main/java/com/star/greendaotest/bean/Thing.java
+++ app/src/main/java/com/star/greendaotest/bean/Thing.java
@@ -1,9 +1,21 @@
 package com.star.greendaotest.bean;
 
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Transient;
 
 public class Thing {
 
     @Id(autoincrement = true)
     private Long id;
+
+    @Property(nameInDb="message")
+    @Index(unique = true)
+    String message;
+
+    String name;
+
+    @Transient
+    long time;
 }
