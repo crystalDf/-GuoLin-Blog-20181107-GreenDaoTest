c7120296f304d22a4b361d2745d68c1a00ac5e8a
crystalDf <chendong333@gmail.com>
2018-12-03 22:58:23 +0800

    GreenDaoTest

--- app/src/main/java/com/star/greendaotest/bean/Thing.java
+++ app/src/main/java/com/star/greendaotest/bean/Thing.java
@@ -39,4 +39,28 @@ public class Thing {
     public Thing() {
 
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
